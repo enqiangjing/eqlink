bb5a1eb8e0eb31d71754bf158e82db8c51d00704
enqiangjing <enqiangjing@163.com>
2022-08-28 15:40:40 +0800

    license

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name="eqlink",
-    version="0.0.15",
+    version="0.0.16",
     keywords=("eqlink", "Registration Center", "eqsmart"),
     description="注册中心",
-    long_description="更新仓库地址",
-    license="GPL-2.0",
+    long_description="更新协议",
+    license="MIT license",
 
     url="https://github.com/enqiangjing/eqlink",
     author="enqiang",
@@ -24,5 +24,5 @@ python setup.py bdist_egg     # 生成类似 eqlink-0.0.1-py2.7.egg，支持 eas
 # 使用此方式
 python setup.py sdist         # 生成类似 eqlink-0.0.1.tar.gz，支持 pip
 # twine 需要安装
-twine upload dist/eqlink-0.0.15.tar.gz
+twine upload dist/eqlink-0.0.16.tar.gz
 """
